9a0ee049e40b31347404828d228165e7eef75b36
Arturious9 <example@gmail.com>
2022-06-14 17:10:09 +0300

    BowWow v.1.01

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let incr = 17,
+        decr = 21;
+
+// ++incr;
+// --decr;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(210%20);
+
+console.log(2 * 2 + 10 !== "14");
+
+const isCucked = false;
+const isFucked = false;
+
+console.log(isCucked || isFucked);
