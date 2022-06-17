88cc4c9367d5a375004e3523b723486b001c94b7
Arturious9 <example@gmail.com>
2022-06-17 21:24:35 +0300

    MovieDB remake

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const numberOfMovies = +prompt("Сколько фильмов вы уже просмотрели?", "");
+
+const personalMovieDB = {
+        count: numberOfMovies,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+};
+
+const a = prompt("Один из просмотренных вами ранее фильмов", ""),
+        b = +prompt("Ваша оценка этому фильму?", ""),
+        c = prompt("Один из просмотренных вами ранее фильмов", ""),
+        d = +prompt("Ваша оценка этому фильму?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
